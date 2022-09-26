8b20fb8ea35469d4c80b5f854b36d536020ab5c2
mdsjjorge <mdsousa.jorge@gmail.com>
2022-09-26 08:16:35 -0300

    search bar funcionando

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { IProduct, } from '../products';
 import { ProductsService } from '../products.service';
 
@@ -11,10 +12,21 @@ export class ProductsComponent implements OnInit {
   products: IProduct[] | undefined;
 
   constructor(
-    private productsService: ProductsService
+    private productsService: ProductsService,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.products = this.productsService.getAll();
+    const products = this.productsService.getAll();
+    this.route.queryParamMap.subscribe(params => {
+      const description = params.get("description")?.toLowerCase();
+
+      if(description) {
+        this.products = products.filter(product => product.description.toLowerCase().includes(description));
+        return;
+      }
+
+      this.products = products;
+    });
   }
 }
